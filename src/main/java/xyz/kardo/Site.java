1493f693e4a4981b58e3153aa464a06abca8ea33
Kardo <kardoj@gmail.com>
2016-10-20 23:16:51 +0300

    Renames config to SiteConfig and adds new Site class which makes the fetch

--- src/main/java/xyz/kardo/Site.java
+++ src/main/java/xyz/kardo/Site.java
@@ -3,15 +3,43 @@
  */
 package xyz.kardo;
 
-public class Config {
-	public int interval;
-	public String title;
-	public boolean sendMails;
-	public String to;
-	public String from;
-	public String subject;
-	public String url;
-	public String selector;
-	public String subSelector;
-	public String dataFile;
+import java.util.ArrayList;
+
+public class Site {
+	private boolean sendMails;
+	private String to;
+	private String from;
+	private String subject;
+	private String url;
+	private String selector;
+	private String subSelector;
+	private String dataFile;
+	
+	private Mailer mailer;
+	private ResultFilter resultFilter;
+	private Crawler crawler;
+	
+	public Site(SiteConfig config) {
+		setAttributes(config);
+		
+		mailer = new Mailer(from, to, subject);
+		resultFilter = new ResultFilter(dataFile);
+		crawler = new Crawler(url, selector, subSelector);
+	}
+	
+	private void setAttributes(SiteConfig config) {
+		this.sendMails = config.sendMails;
+		this.to = config.to;
+		this.from = config.from;
+		this.subject = config.subject;
+		this.url = config.url;
+		this.selector = config.selector;
+		this.subSelector = config.subSelector;
+		this.dataFile = config.dataFile;		
+	}
+	
+	public void check() {
+		ArrayList<String> newLinks = resultFilter.filter(crawler.crawl());
+		if (sendMails) mailer.sendEmails(newLinks);		
+	}
 }
---
+++ src/main/java/xyz/kardo/SiteConfig.java
@@ -0,0 +1,15 @@
+/*
+ * Kardo Jõeleht 2016
+ */
+package xyz.kardo;
+
+public class SiteConfig {
+	public boolean sendMails;
+	public String to;
+	public String from;
+	public String subject;
+	public String url;
+	public String selector;
+	public String subSelector;
+	public String dataFile;
+}
